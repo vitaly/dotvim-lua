8eb654860ff77869631fab923c2158192bfba7ce
Vitaly Kushner <vitaly@astrails.com>
2021-11-13 08:22:33 +0100

    m

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -13,6 +13,52 @@ plugin('my.plugins.fuzzy')
 plugin('my.plugins.lsp')
 plugin('my.plugins.cmp')
 
+
+use {
+  'vitaly/vim-tmux-navigator',
+  config = function ()
+    vim.cmd [[
+      noremap <silent> <plug>(Window/Up)    :TmuxNavigateUp<CR>
+      noremap <silent> <plug>(Window/Down)  :TmuxNavigateDown<CR>
+      noremap <silent> <plug>(Window/Left)  :TmuxNavigateLeft<CR>
+      noremap <silent> <plug>(Window/Right) :TmuxNavigateRight<CR>
+
+      nmap <leader>wk <plug>(Window/Up)
+      nmap <leader>wj <plug>(Window/Down)
+      nmap <leader>wh <plug>(Window/Left)
+      nmap <leader>wl <plug>(Window/Right)
+
+      cmap <silent> <m-k> :<C-U>TmuxNavigateUp<CR>
+      cmap <silent> <m-j> :<C-U>TmuxNavigateDown<CR>
+      cmap <silent> <m-h> :<C-U>TmuxNavigateLeft<CR>
+      cmap <silent> <m-l> :<C-U>TmuxNavigateRight<CR>
+
+      tmap <m-k> <C-\><C-n><plug>(Window/Up)
+      tmap <m-j> <C-\><C-n><plug>(Window/Down)
+      tmap <m-h> <C-\><C-n><plug>(Window/Left)
+      tmap <m-l> <C-\><C-n><plug>(Window/Right)
+    ]]
+  end
+
+}
+
+use {
+  'mbbill/undotree',
+  config = function ()
+    vim.cmd [[
+      noremap             <plug>(Toggle/Unfo-Tree) :UndotreeToggle<CR>
+      nmap    <leader>Tu  <plug>(Toggle/Unfo-Tree)
+
+      " alias for quick access
+      map                 <plug>(Toggle-Unfo-Tree) <plug>(Toggle/Unfo-Tree)
+      nmap <leader>u      <plug>(Toggle-Unfo-Tree)
+
+      nmap g= :later<cr>
+   ]]
+  end
+}
+
+
 puts 'my.plugins loaded'
 
 return plugins
