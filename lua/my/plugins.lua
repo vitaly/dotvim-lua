15dec959951c08b46359c7ed2e5d629b4aaf843d
Vitaly Kushner <vitaly@astrails.com>
2021-11-14 11:41:16 +0100

    interestingwords

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -16,6 +16,14 @@ use 'tpope/vim-surround'            -- https://github.com/tpope/vim-surround
 use 'editorconfig/editorconfig-vim'
 
 use {
+  'lfv89/vim-interestingwords',
+  setup = function ()
+    vim.g.interestingWordsGUIColors = { '#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff', '#888888', '#880000', '#008800', '#000088', '#888800', '#008888', '#880088', '#8800ff', '#88aaff', '#ff8800', '#ff4488', '#ffffff' }
+    -- vim.g.interestingWordsRandomiseColors = 1
+  end
+}
+
+use {
   'junegunn/vim-easy-align', -- https://github.com/junegunn/vim-easy-align
   config = function()
     xmap('<Enter>', '<Plug>(LiveEasyAlign)')
