c8e2c1cb86ca71566c9f65d8c34c009def3b8735
Vitaly Kushner <vitaly@astrails.com>
2021-11-14 06:18:31 +0100

    commenrts

--- lua/my/plugins/comments.lua
+++ lua/my/plugins/comments.lua
@@ -1,8 +1,19 @@
 return function (use)
+  -- XXX: TODO: TBD: lazy load
   use {
     'terrortylor/nvim-comment', -- https://github.com/terrortylor/nvim-comment
     config = function ()
       require('nvim_comment').setup()
+
+      local vimp = require('vimp')
+      local noremap = vimp.nmap
+
+      noremap('<plug>Comment(toggle)', '<cmd>CommentToggle<cr>')
+
+      local nmap = vimp.nmap
+      nmap('<leader>;', '<plug>Comment(toggle)')
+      local xmap = vimp.xmap
+      xmap('<leader>;', ':CommentToggle<cr>')
     end
   }
 end
