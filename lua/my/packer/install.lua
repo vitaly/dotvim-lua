aa1ac3d32d276e118fc04bd1d2053e5aa1d252ce
Vitaly Kushner <vitaly@astrails.com>
2021-11-15 16:40:37 +0100

    autoinstall

--- lua/my/packer/install.lua
+++ lua/my/packer/install.lua
@@ -40,8 +40,9 @@ end
 vim.cmd([[
   augroup packer_plugins
     autocmd!
-    autocmd BufWritePost */my/plugins.lua PackerCompile
-    autocmd BufWritePost */my/plugins/*.lua PackerCompile
+    autocmd BufWritePost */my/plugins.lua PackerInstall
+    autocmd BufWritePost */my/plugins/*.lua PackerInstall
+    autocmd User PackerComplete PackerCompile
     autocmd User PackerCompileDone checktime
   augroup end
 ]])
