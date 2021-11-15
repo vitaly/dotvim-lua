84f1d1983aa279eb95967018b426cb0682e86636
Vitaly Kushner <vitaly@astrails.com>
2021-11-15 15:35:34 +0100

    lualine

--- lua/my/plugins/statusline.lua
+++ lua/my/plugins/statusline.lua
@@ -1,13 +1,27 @@
 return function (use)
-  use({
-    "NTBBloodbath/galaxyline.nvim",
-    -- your statusline
-    config = function()
-      -- require("galaxyline.themes.eviline")
-      -- require("galaxyline.themes.neonline")
-      require("galaxyline.themes.spaceline")
-    end,
-    -- some optional icons
-    requires = { "kyazdani42/nvim-web-devicons", opt = true }
-  })
+  -- use {
+  --   "NTBBloodbath/galaxyline.nvim",
+  --   -- your statusline
+  --   config = function()
+  --     -- require("galaxyline.themes.eviline")
+  --     -- require("galaxyline.themes.neonline")
+  --     require("galaxyline.themes.spaceline")
+  --   end,
+  --   -- some optional icons
+  --   requires = { "kyazdani42/nvim-web-devicons" },
+  -- }
+
+  use {
+    'nvim-lualine/lualine.nvim', -- https://github.com/nvim-lualine/lualine.nvim
+    requires = { "kyazdani42/nvim-web-devicons" },
+
+    config = function ()
+      require('lualine').setup({
+        options = {
+          -- theme = vim.g.colors_name or 'auto',
+
+        },
+      })
+    end
+  }
 end
