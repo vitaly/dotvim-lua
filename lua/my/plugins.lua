ef2509c66208227c3993b4ee88d8670930baec70
Vitaly Kushner <vitaly@astrails.com>
2021-11-15 13:49:30 +0100

    diminactive

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -54,6 +54,23 @@ plugin('my.plugins.zoom')
 plugin('my.plugins.statusline')
 plugin('my.plugins.tags')
 
+use {
+  'blueyed/vim-diminactive', -- https://github.com/blueyed/vim-diminactive
+
+  setup = function()
+    vim.g.diminactive_use_colorcolumn = 1
+    vim.g.diminactive_use_syntax = 0
+  end,
+
+  config = function ()
+    noremap('<plug>Toggle(diminactive)', function ()
+      vim.cmd[[DimInactiveToggle]]
+    end)
+
+    nmap('<leader>Td', '<plug>Toggle(diminactive)')
+  end,
+}
+
 
 -- puts 'my.plugins loaded'
 return pinit
