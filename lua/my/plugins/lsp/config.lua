a64ea76065411466d2763923b6add8acee05603e
Vitaly Kushner <vitaly@astrails.com>
2021-11-17 21:24:53 +0100

    diags

--- lua/my/plugins/lsp/config.lua
+++ lua/my/plugins/lsp/config.lua
@@ -1,10 +1,19 @@
--- local lspkind = require('lspkind')
+local signs = { Error = '✖', Warn = '', Hint = '', Info = '' }
+
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
+  -- virtual_text = false,
+  virtual_text = {
+    prefix = '',
+    spacing = 5,
+  },
   signs = true,
-  update_in_insert = false,
   underline = true,
+  update_in_insert = false,
 })
 
 local on_attach = REQUIRE 'my.plugins.lsp.on-attach'
