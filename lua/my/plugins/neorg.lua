da7af33c55f2cded10794aa9e1074543dee15531
Vitaly Kushner <vitaly@astrails.com>
2021-12-04 16:37:54 +0100

    neorg

--- lua/my/plugins/neorg.lua
+++ lua/my/plugins/neorg.lua
@@ -10,7 +10,7 @@ return {
         ['core.norg.dirman'] = { -- Manage your directories with Neorg
           config = {
             workspaces = {
-              my_workspace = '~/neorg',
+              my_workspace = '~/Documents/neorg',
             },
           },
         },
