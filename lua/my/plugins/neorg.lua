51fe875daaf39df8d1f46ff17db65331c045ee81
Vitaly Kushner <vitaly@astrails.com>
2021-12-05 13:12:46 +0100

    fix smart search

--- lua/my/plugins/neorg.lua
+++ lua/my/plugins/neorg.lua
@@ -3,6 +3,7 @@ return {
   'nvim-neorg/neorg',
   requires = 'nvim-lua/plenary.nvim',
   config = function()
+    print 'NORG CONFIG'
     require('neorg').setup {
       -- Tell Neorg what modules to load
       load = {
@@ -27,14 +28,55 @@ return {
       },
     }
 
-    require('which-key').register {
-      ['<leader>o'] = {
-        name = 'NeOrg',
+    local neorg_callbacks = require 'neorg.callbacks'
 
-        m = {
-          name = 'Mode',
+    neorg_callbacks.on_event('core.keybinds.events.enable_keybinds', function(_, keybinds)
+      -- Map all the below keybinds only when the "norg" mode is active
+      print 'NORG CONFIG MAPS'
+      keybinds.map_event_to_mode('norg', {
+        n = { -- Bind keys in normal mode
+
+          -- Keys for managing TODO items and setting their states
+          { '<localleader>td', 'core.norg.qol.todo_items.todo.task_done' },
+          { '<localleader>tu', 'core.norg.qol.todo_items.todo.task_undone' },
+          { '<localleader>tp', 'core.norg.qol.todo_items.todo.task_pending' },
+          { '<localleader>tc', 'core.norg.qol.todo_items.todo.task_cycle' },
+          { '<C-Space>', 'core.norg.qol.todo_items.todo.task_cycle' },
         },
+      }, { silent = true, noremap = true })
+    end)
+
+    require('which-key').register {
+      ['<localleader>t'] = {
+        name = 'TODO',
+        x = { '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_cycle<CR>', 'Cycle' },
       },
     }
+
+    -- require('which-key').register {
+    --   ['<leader>o'] = {
+    --     name = 'NeOrg',
+
+    --     m = {
+    --       name = 'Mode',
+    --     },
+    --   },
+    --   ['<localleader>t'] = {
+    --     name = 'Todo',
+
+    --     d = {
+    --       name = 'Done',
+    --     },
+    --     u = {
+    --       name = 'Undone',
+    --     },
+    --     p = {
+    --       name = 'Pending',
+    --     },
+    --     c = {
+    --       name = 'Cycle',
+    --     },
+    --   },
+    -- }
   end,
 }
