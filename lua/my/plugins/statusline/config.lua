1ec6a5b54cbf823356b8007595bf3fe63d58b495
Vitaly Kushner <vitaly@astrails.com>
2021-12-11 17:10:06 +0100

    diagnostic

--- lua/my/plugins/statusline/config.lua
+++ lua/my/plugins/statusline/config.lua
@@ -66,7 +66,7 @@ require('lualine').setup {
   },
 
   sections = {
-    lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_lsp' } } },
+    lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
     lualine_c = { { 'filename', path = 1 }, { dap_status, color = { bg = '#ff0000' } } },
     lualine_x = { 'encoding', 'fileformat', 'filetype', { lsp_clients } },
   },
