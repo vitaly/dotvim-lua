2c4139109bcaff9825294ac3792998ef9e9fa69c
Vitaly Kushner <vitaly@astrails.com>
2021-12-11 18:15:50 +0100

    cmp cleanup

--- lua/my/plugins/cmp.lua
+++ lua/my/plugins/cmp.lua
@@ -2,28 +2,20 @@ return {
   'hrsh7th/nvim-cmp', -- https://github.com/hrsh7th/nvim-cmp
   requires = {
 
-    -- 'honza/vim-snippets',
-    {
-      'rafamadriz/friendly-snippets',
-      event = 'InsertEnter',
-    },
+    { 'windwp/nvim-autopairs' }, -- https://github.com/windwp/nvim-autopairs
+    { 'onsails/lspkind-nvim' }, -- https://github.com/onsails/lspkind-nvim
 
-    'hrsh7th/vim-vsnip', -- https://github.com/hrsh7th/vim-vsnip
+    -- 'honza/vim-snippets',
+    { 'rafamadriz/friendly-snippets', event = 'InsertEnter' },
+    { 'hrsh7th/vim-vsnip' }, -- https://github.com/hrsh7th/vim-vsnip
     { 'hrsh7th/cmp-vsnip', after = 'nvim-cmp' },
     -- 'L3MON4D3/LuaSnip', -- https://github.com/L3MON4D3/LuaSnip
     -- { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
 
     { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-    'hrsh7th/cmp-nvim-lsp',
     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
-    {
-      'windwp/nvim-autopairs', -- https://github.com/windwp/nvim-autopairs
-      -- after = 'nvim-cmp',
-    },
-    {
-      'onsails/lspkind-nvim', -- https://github.com/onsails/lspkind-nvim
-    },
+    { 'hrsh7th/cmp-nvim-lsp' },
   },
 
   config = function()
