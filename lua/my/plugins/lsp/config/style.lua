116ed938bff6046c3b21f0861f4bd1ab2299e397
Vitaly Kushner <vitaly@astrails.com>
2022-01-11 06:48:11 +0100

    lspconfig: border popup

--- lua/my/plugins/lsp/config/style.lua
+++ lua/my/plugins/lsp/config/style.lua
@@ -13,3 +13,13 @@ end
 vim.cmd [[
   hi DiagnosticHint guifg=yellow
 ]]
+
+-- set double border by default
+local win = require 'lspconfig.ui.windows'
+local _default_opts = win.default_opts
+
+win.default_opts = function(options)
+  local opts = _default_opts(options)
+  opts.border = 'double'
+  return opts
+end
