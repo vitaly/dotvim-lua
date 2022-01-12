0429ce69590bf6e62ee65783cba0e3646a9c25b2
Vitaly Kushner <vitaly@astrails.com>
2022-01-12 05:20:27 +0100

    fuzzy

--- lua/my/plugins/fuzzy/init.lua
+++ lua/my/plugins/fuzzy/init.lua
@@ -21,63 +21,8 @@ return {
       'telescope-fzf-native.nvim',
     },
 
-    setup = function()
-      REQUIRE 'my.plugins.fuzzy.maps'
-    end,
-
     config = function()
-      local telescope = require 'telescope'
-      local actions = require 'telescope.actions'
-      telescope.setup {
-        defaults = {
-          layout_strategy = 'flex',
-          scroll_strategy = 'cycle',
-          mappings = {
-            i = {
-              ['<esc>'] = actions.close,
-              ['<C-j>'] = actions.move_selection_next,
-              ['<C-k>'] = actions.move_selection_previous,
-            },
-          },
-        },
-        extensions = {
-          fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = 'smart_case',
-          },
-          fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-          },
-        },
-        pickers = {
-          help_tags = { theme = 'ivy' },
-          git_files = { theme = 'ivy' },
-          find_files = { theme = 'ivy' },
-          commands = {
-            theme = 'dropdown',
-            previewer = false,
-          },
-          buffers = {
-            theme = 'ivy',
-            sort_lastused = true,
-          },
-          lsp_references = { theme = 'ivy' },
-          lsp_code_actions = { theme = 'dropdown' },
-          lsp_definitions = { theme = 'ivy' },
-          lsp_implementations = { theme = 'ivy' },
-        },
-      }
-
-      telescope.load_extension 'fzf'
-      telescope.load_extension 'fzy_native'
-      telescope.load_extension 'dap'
-
-      -- puts 'telescope configured'
+      require 'my.plugins.fuzzy.config'
     end,
-    cmd = 'Telescope',
-    module = 'telescope',
   },
 }
