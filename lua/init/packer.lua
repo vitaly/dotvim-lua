5964d95cbf3d050c7df047173caf62a0f72c1a84
Vitaly Kushner <vitaly@astrails.com>
2022-09-19 14:17:04 +0200

    plugins

--- lua/init/packer.lua
+++ lua/init/packer.lua
@@ -4,6 +4,7 @@ vim.cmd [[packadd packer.nvim]]
 
 local packer = require 'packer'
 
+local border = 'rounded'
 packer.startup({
   function(use)
     use { 'wbthomason/packer.nvim', opt = true }
@@ -12,10 +13,51 @@ packer.startup({
     use 'nvim-lua/plenary.nvim' -- https://github.com/nvim-lua/plenary.nvim
 
     use 'folke/which-key.nvim' -- https://github.com/folke/which-key.nvim
+
+    use {
+      {
+        'nvim-telescope/telescope-fzf-native.nvim', -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+        run = 'make',
+      },
+      {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+          'nvim-lua/popup.nvim',
+          'nvim-lua/plenary.nvim',
+          'telescope-fzf-native.nvim',
+          'nvim-telescope/telescope-fzy-native.nvim',
+          'nvim-telescope/telescope-symbols.nvim',
+          'nvim-telescope/telescope-dap.nvim', -- https://github.com/nvim-telescope/telescope-dap.nvim
+        },
+
+        wants = {
+          'popup.nvim',
+          'plenary.nvim',
+          'telescope-fzf-native.nvim',
+        },
+
+        config = function()
+          -- require 'my.plugins.fuzzy.config'
+        end,
+      },
+    }
+
   end,
+
   config = {
     package_root = vim.fn.stdpath 'config' .. '/pack',
 
+    profile = {
+      enable = true,
+      threshold = 0,
+    },
+
+    display = {
+      open_fn = function()
+        return require('packer.util').float { border = border }
+      end,
+      prompt_border = border,
+    },
   },
 })
 
