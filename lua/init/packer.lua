67fde63723e80bb61fdbb7339a979ba04aeb89e9
Vitaly Kushner <vitaly@astrails.com>
2022-09-21 22:32:15 +0200

    cosmetic

--- lua/init/packer.lua
+++ lua/init/packer.lua
@@ -1,15 +1,16 @@
 -- just copied those form the packer sources, replacing require('packer') with require('plugins')
-vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.create PackerSnapshot  lua require('plugins').snapshot(<f-args>)]]
-vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.rollback PackerSnapshotRollback  lua require('plugins').rollback(<f-args>)]]
-vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.snapshot PackerSnapshotDelete lua require('plugins.snapshot').delete(<f-args>)]]
-vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete PackerInstall lua require('plugins').install(<f-args>)]]
-vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete PackerUpdate lua require('plugins').update(<f-args>)]]
-vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete PackerSync lua require('plugins').sync(<f-args>)]]
-vim.cmd [[command! PackerClean             lua require('plugins').clean()]]
-vim.cmd [[command! -nargs=* PackerCompile  lua require('plugins').compile(<q-args>)]]
-vim.cmd [[command! PackerStatus            lua require('plugins').status()]]
-vim.cmd [[command! PackerProfile           lua require('plugins').profile_output()]]
-vim.cmd [[command! -bang -nargs=+ -complete=customlist,v:lua.require'packer'.loader_complete PackerLoad lua require('plugins').loader(<f-args>, '<bang>' == '!')]]
+--
+vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.create   PackerSnapshot          lua require('plugins').snapshot(<f-args>)                ]]
+vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.rollback PackerSnapshotRollback  lua require('plugins').rollback(<f-args>)                ]]
+vim.cmd [[command! -nargs=+ -complete=customlist,v:lua.require'packer.snapshot'.completion.snapshot PackerSnapshotDelete    lua require('plugins.snapshot').delete(<f-args>)         ]]
+vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete              PackerInstall           lua require('plugins').install(<f-args>)                 ]]
+vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete              PackerUpdate            lua require('plugins').update(<f-args>)                  ]]
+vim.cmd [[command! -nargs=* -complete=customlist,v:lua.require'packer'.plugin_complete              PackerSync              lua require('plugins').sync(<f-args>)                    ]]
+vim.cmd [[command!                                                                                  PackerClean             lua require('plugins').clean()                           ]]
+vim.cmd [[command! -nargs=*                                                                         PackerCompile           lua require('plugins').compile(<q-args>)                 ]]
+vim.cmd [[command!                                                                                  PackerStatus            lua require('plugins').status()                          ]]
+vim.cmd [[command!                                                                                  PackerProfile           lua require('plugins').profile_output()                  ]]
+vim.cmd [[command! -bang -nargs=+ -complete=customlist,v:lua.require'packer'.loader_complete        PackerLoad              lua require('plugins').loader(<f-args>, '<bang>' == '!') ]]
 
 if _my.packer.updated then
   -- packer was just initially installed
