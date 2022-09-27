9506a280650069912b536915f53f4a1849e0c490
Vitaly Kushner <vitaly@astrails.com>
2022-09-28 00:30:13 +0200

    colorizer fixes

--- lua/init/tools/ui.lua
+++ lua/init/tools/ui.lua
@@ -3,8 +3,14 @@ local M = {}
 -- take background from one highlight and apply it to the other
 function M.hl_bg_copy(from, to, extra)
   extra = extra or ''
-  local hl = 'hi ' .. to .. ' guibg=' .. vim.fn.synIDattr(vim.fn.hlID(from), 'bg') .. ' ' .. extra
-  vim.cmd(hl)
+  local bg = vim.fn.synIDattr(vim.fn.hlID(from), 'bg')
+  if bg ~= '' then
+    local hl = 'hi ' .. to .. ' guibg=' .. bg .. ' ' .. extra
+    -- vim.notify(hl, vim.log.levels.DEBUG)
+    vim.cmd(hl)
+  else
+    vim.notify(string.format('no bg for %s', from), vim.log.levels.WARN)
+  end
 end
 
 -- make sure that the given highlight has background equal to that of SignColumn
--- lua/layers/editor/init.lua
+++ lua/layers/editor/init.lua
@@ -75,29 +75,7 @@ return {
     'NvChad/nvim-colorizer.lua', -- https://github.com/NvChad/nvim-colorizer.lua
 
     config = function()
-      local mode_toggle = require('lib.toggle').toggler('g:colorizer_mode', { 'virtualtext', 'foreground', 'background' })
-
-      require('colorizer').setup { user_default_options = { mode = 'foreground' } }
-
-      require('which-key').register {
-        ["\\'"] = {
-          c = {
-            function()
-              local c = require 'colorizer'
-              if c.is_buffer_attached(0) then
-                c.detach_from_buffer(0)
-              else
-                local mode = vim.g.colorizer_mode or 'foreground'
-                PRINT { 'COLORIZE', mode }
-                c.attach_to_buffer(0, { mode = mode })
-              end
-            end,
-            'Colorizer',
-          },
-
-          C = { mode_toggle, 'Colorizer Color' },
-        },
-      }
+      require('layers.editor.colorizer').config()
     end,
   },
 
