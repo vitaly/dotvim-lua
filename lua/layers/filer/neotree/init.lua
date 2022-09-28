59efb01fd7b707cb59d82e606874724fa48cc9ae
Vitaly Kushner <vitaly@astrails.com>
2022-09-28 10:08:51 +0200

    neotree

--- lua/layers/filer/neotree/init.lua
+++ lua/layers/filer/neotree/init.lua
@@ -26,49 +26,6 @@ return {
   end,
 
   config = function()
-    vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
-
-    local side = _my.config.filer.side or 'left'
-    -- DEFAULTS: pack/packer/opt/neo-tree.nvim/lua/neo-tree/defaults.lua
-    require('neo-tree').setup {
-
-      source_selector = {
-        winbar = true,
-      },
-
-      use_popups_for_input = false, -- If false, inputs will use vim.ui.input() instead of custom floats.
-      close_if_last_window = true,
-      sort_case_insensitive = true,
-
-      filesystem = {
-        group_empty_dirs = true,
-        follow_current_file = true,
-      },
-
-      default_component_configs = {
-        icon = {
-          default = '',
-          folder_empty = '',
-        },
-        git_status = {
-          symbols = {
-            modified = '',
-            deleted = '',
-            staged = '✓',
-            unstaged = '✗',
-            untracked = '?',
-          },
-        },
-      }, -- default_component_configs
-
-      window = {
-        position = side,
-        mappings = {
-          ['r'] = { 'move', config = { show_path = 'relative' } }, -- no need not to allow to move for rename ;)
-          ['m'] = { 'move', config = { show_path = 'relative' } },
-          ['<esc>'] = 'close_window',
-        },
-      },
-    } -- setup
+    require('layers.filer.neotree.config').config()
   end,
 }
