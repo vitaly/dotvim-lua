1dec4e8a397ce133ff019fa0540e02090c33c183
Vitaly Kushner <vitaly@astrails.com>
2022-10-06 07:07:39 +0200

    lualine config

--- lua/layers/ui/statusline/lua_line/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-lualine/lualine.nvim', -- https://github.com/nvim-lualine/lualine.nvim
-  requires = { 'kyazdani42/nvim-web-devicons' },
-
-  config = function()
-    require('layers.ui.statusline.lua_line.config').setup()
-  end,
-}
