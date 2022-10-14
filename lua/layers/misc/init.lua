191dd736d648e5e076ceb71083a3c503fcab8536
Vitaly Kushner <vitaly@astrails.com>
2022-10-14 13:34:13 +0200

    misc

--- lua/layers/misc/init.lua
+++ lua/layers/misc/init.lua
@@ -3,30 +3,39 @@ return {
   {
     'guns/xterm-color-table.vim', -- https://github.com/guns/xterm-color-table.vim
     config = function()
-      require('layers.misc.config').setup()
+      vim.g.XtermColorTableDefaultOpen = 'edit'
+
+      require('which-key').register {
+        ['<leader>'] = {
+          sx = { '<cmd>XtermColorTable<cr>', 'Xtem Color Table' },
+        },
+      }
     end,
   },
-  {
-    'fgheng/winbar.nvim', -- https://github.com/fgheng/winbar.nvim
 
-    after = {
-      'nvim-base16',
-    },
+  require 'layers.misc.foo',
 
-    config = function()
-      _my.au.on_colorscheme('winbar.style', function()
-        local colors = require('base16-colorscheme').colors
-        require('winbar').setup {
-          enabled = true,
-          show_file_path = true,
+  -- {
+  --   'fgheng/winbar.nvim', -- https://github.com/fgheng/winbar.nvim
 
-          -- for ref: pack/packer/start/base16-vim/colors/base16-ia-light.vim
-          colors = {
-            path = colors.base0D,
-            file_name = colors.base08,
-          },
-        }
-      end)
-    end,
-  },
+  --   after = {
+  --     'nvim-base16',
+  --   },
+
+  --   config = function()
+  --     _my.au.on_colorscheme('winbar.style', function()
+  --       local colors = require('base16-colorscheme').colors
+  --       require('winbar').setup {
+  --         enabled = true,
+  --         show_file_path = true,
+
+  --         -- for ref: pack/packer/start/base16-vim/colors/base16-ia-light.vim
+  --         colors = {
+  --           path = colors.base0D,
+  --           file_name = colors.base08,
+  --         },
+  --       }
+  --     end)
+  --   end,
+  -- },
 }
