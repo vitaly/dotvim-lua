6cb18a8547ac0d72ff5dd21346512053e226adba
Vitaly Kushner <vitaly@astrails.com>
2023-06-23 01:13:24 +0200

    _my.au.group

--- lua/init/tools/au.lua
+++ lua/init/tools/au.lua
@@ -1,37 +1,46 @@
-local M = {}
+local AU = {}
 
-function M.command(event, opts)
+-- if 'clear' is true, will clear the group if it already exists
+-- clear is true by default
+function AU.group(name, clear)
+  if clear == nil then
+    clear = true
+  end
+  return vim.api.nvim_create_augroup(name, { clear = clear })
+end
+
+function AU.command(event, opts)
   opts.group = opts.group or error('au.command requires a group', 2)
   opts.callback = opts.callback or error('au.command requires a callback', 2)
 
   if 'string' == type(opts.group) then
-    opts.group = vim.api.nvim_create_augroup(opts.group, { clear = true })
+    opts.group = AU.group(opts.group)
   end
 
   return vim.api.nvim_create_autocmd(event, opts)
 end
 
-function M.callback(group, event, callback, opts)
+function AU.callback(group, event, callback, opts)
   opts = opts or {}
   opts.group = group
   opts.callback = callback
 
-  return M.command(event, opts)
+  return AU.command(event, opts)
 end
 
-function M.on_colorscheme(group, callback, opts)
+function AU.on_colorscheme(group, callback, opts)
   callback()
-  return M.callback(group, 'ColorScheme', callback, opts)
+  return AU.callback(group, 'ColorScheme', callback, opts)
 end
 
-function M.on_lsp_attach(group, callback, opts)
+function AU.on_lsp_attach(group, callback, opts)
   -- PRINT { 'on_lsp_attach', group, callback, opts }
-  return M.callback(group, 'LspAttach', function(args)
+  return AU.callback(group, 'LspAttach', function(args)
     callback(vim.lsp.get_client_by_id(args.data.client_id), args.buf)
   end, opts)
 end
 
-function M.buffer_callback(buffer, group, event, callback, opts)
+function AU.buffer_callback(buffer, group, event, callback, opts)
   opts = opts or {}
   opts.buffer = buffer or error('missing buffer', 2)
 
@@ -43,7 +52,7 @@ function M.buffer_callback(buffer, group, event, callback, opts)
 
   opts.callback = callback
 
-  return M.command(event, opts)
+  return AU.command(event, opts)
 end
 
-return M
+return AU
