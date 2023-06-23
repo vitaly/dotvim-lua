a01d8f33c97cd76d189126ad4b46ce83d8deb4b6
Vitaly Kushner <vitaly@astrails.com>
2023-06-23 04:06:24 +0200

    diffmode colors

--- lua/layers/base/colors.lua
+++ lua/layers/base/colors.lua
@@ -7,6 +7,25 @@ return {
   'RRethy/nvim-base16', -- https://github.com/RRethy/nvim-base16
 
   config = function()
+    -- I don't like the way base16 colorschemes setup diffmode styling
+    -- let's make sure that added/deleted/changed lines have a green/red/blue background
+    local function diffmode_colors()
+      local base16 = require 'base16-colorscheme'
+      local colors = base16.colors
+      local hi = base16.highlight
+
+      if colors then
+        -- 'colors' only available when a base16 colorscheme is set in vim
+        hi.DiffDelete = { guibg = colors.base00, guifg = colors.base08, gui = nil, guisp = nil }
+        hi.DiffAdd = { guibg = colors.base0B, guifg = colors.base00, gui = nil, guisp = nil }
+        hi.DiffChange = { guibg = colors.base0A, guifg = colors.base00, gui = nil, guisp = nil }
+        hi.DiffText = { guibg = colors.base0B, guifg = colors.base00, gui = nil, guisp = nil }
+      end
+    end
+
+    _my.au.on_colorscheme('diffmode.colors', diffmode_colors, { pattern = 'base16-*' })
+
+    -- command to (re)load vim background from `~/.vimrc_background`
     cmdbang('LoadVimrcBackground', function()
       vim.cmd [[
         if filereadable(expand("~/.vimrc_background"))
@@ -15,6 +34,7 @@ return {
       ]]
     end)
 
+    -- load vim background on startup
     vim.cmd [[LoadVimrcBackground]]
 
     nnoremap('<plug>(Vim/Update-Colors)', ':LoadVimrcBackground<CR>')
