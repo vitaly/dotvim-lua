c33877dafa6170a3a3e1eacc9395c8076460eaaa
Vitaly Kushner <vitaly@astrails.com>
2023-06-28 05:19:55 +0200

    use vim.keymap.set

--- lua/plugins/base/keymaps/diff-mode-maps.lua
+++ lua/plugins/base/keymaps/diff-mode-maps.lua
@@ -2,15 +2,16 @@ return {
   setup = function()
     -- add diffmode keymaps
     local diff_mode = function()
-      vim.api.nvim_buf_set_keymap(0, 'n', ',dp', '<cmd>diffput<cr>', { noremap = true, silent = true, desc = 'Diff Put' })
-      vim.api.nvim_buf_set_keymap(0, 'n', ',dg', '<cmd>diffget<cr>', { noremap = true, silent = true, desc = 'Diff Get' })
-      vim.api.nvim_buf_set_keymap(0, 'n', ',dn', ']c', { noremap = true, silent = true, desc = 'Next Diff' })
-      vim.api.nvim_buf_set_keymap(0, 'n', ',dN', '[c', { noremap = true, silent = true, desc = 'Prev Diff' })
+      vim.keymap.set('n', ',dp', '<cmd>diffput<cr>', { buffer = 0, silent = true, desc = 'Diff Put' })
+      vim.keymap.set('n', ',dg', '<cmd>diffget<cr>', { buffer = 0, silent = true, desc = 'Diff Get' })
+      vim.keymap.set('n', ',dn', ']c', { buffer = 0, silent = true, desc = 'Next Diff' })
+      vim.keymap.set('n', ',dN', '[c', { buffer = 0, silent = true, desc = 'Prev Diff' })
+
       if not vim.o.diff then
-        vim.api.nvim_buf_del_keymap(0, 'n', ',dp')
-        vim.api.nvim_buf_del_keymap(0, 'n', ',dg')
-        vim.api.nvim_buf_del_keymap(0, 'n', ',dn')
-        vim.api.nvim_buf_del_keymap(0, 'n', ',dN')
+        vim.keymap.del('n', ',dp', { buffer = 0 })
+        vim.keymap.del('n', ',dg', { buffer = 0 })
+        vim.keymap.del('n', ',dn', { buffer = 0 })
+        vim.keymap.del('n', ',dN', { buffer = 0 })
       end
     end
 
