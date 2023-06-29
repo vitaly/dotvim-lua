6703ba1bfd41d6b563986b337e01ca1d3ddccef6
Vitaly Kushner <vitaly@astrails.com>
2023-06-29 14:35:00 +0200

    use plugin for last file location

--- lua/init/autocommands.lua
+++ lua/init/autocommands.lua
@@ -1,20 +1,6 @@
 local au = require 'lib.au'
 
 local setup_autocommands = function()
-  -- restore cursor position when opening files
-  au.command('last-location', 'BufReadPost', function()
-    local ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
-    -- do not restore cursor for these filetypes
-    if ft == 'gitcommit' or ft == 'gitrebase' then
-      return
-    end
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end)
-
   -- Highlight on yank
   au.command('highlight.on_yank', 'TextYankPost', function()
     vim.highlight.on_yank { timeout = 200, on_visual = false }
--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -4,4 +4,7 @@ return {
     'jrudess/vim-foldtext', -- https://github.com/jrudess/vim-foldtext
     event = { 'BufReadPost', 'BufNewFile' },
   },
+
+  -- REMEMBER LAST FILE LOCATION
+  { 'ethanholz/nvim-lastplace', opts = {} } -- https://github.com/ethanholz/nvim-lastplace
 }
