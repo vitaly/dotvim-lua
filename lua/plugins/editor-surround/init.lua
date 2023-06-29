974a53fae4501b30358fa38ed19d86c34d943d7a
Vitaly Kushner <vitaly@astrails.com>
2023-06-29 16:35:35 +0200

    add_visual surround mapping

--- lua/plugins/editor-surround/init.lua
+++ lua/plugins/editor-surround/init.lua
@@ -3,6 +3,11 @@ return {
   'echasnovski/mini.surround', -- https://github.com/echasnovski/mini.surround
   version = '*',
   opts = {
-    mappings = { add = 'ys', delete = 'ds', replace = 'cs' },
+    mappings = { add = 'ys', add_visual = 'S', delete = 'ds', replace = 'cs' },
   },
+  config = function(_, opts)
+    require('mini.surround').setup(opts)
+    vim.keymap.del('x', opts.mappings.add)
+    vim.keymap.set('x', opts.mappings.add_visual, [[:<c-u>lua MiniSurround.add('visual')<cr>]], { silent = true, desc = 'Add surrounding to selection' })
+  end,
 }
