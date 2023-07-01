18a2131a75bd50d6689065df0e9628e7e4c90152
Vitaly Kushner <vitaly@astrails.com>
2023-07-01 21:15:53 +0200

    separate lsp-null

---
+++ lua/plugins/lsp-null/init.lua
@@ -0,0 +1,47 @@
+---------------------------------------------------------------------------------
+-- NULL_LS
+-- generic LSP server used for things like linting, formatting, etc
+---------------------------------------------------------------------------------
+return {
+  'jose-elias-alvarez/null-ls.nvim', -- https://github.com/jose-elias-alvarez/null-ls.nvim
+  dependencies = {
+    'jayp0521/mason-null-ls.nvim', -- https://github.com/jayp0521/mason-null-ls.nvim
+  },
+  event = { 'BufReadPre', 'BufNewFile' },
+
+  keys = {
+    { [[<leader>Sn]], vim.cmd.NullLsInfo, desc = 'Null LS Info' },
+    { [[<leader>ani]], vim.cmd.NullLsInfo, desc = 'Null LS Info' },
+    { [[<leader>anl]], vim.cmd.NullLsLog, desc = 'Null LS Log' },
+  },
+
+  opts = function()
+    local nls = require 'null-ls'
+    return {
+      border = 'single',
+      debug = false,
+
+      -- root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', 'Makefile', '.git'),
+      sources = {
+        nls.builtins.formatting.prettier,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.rubocop,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.diagnostics.rubocop,
+        nls.builtins.diagnostics.shellcheck,
+        -- nls.builtins.completion.spell,
+        -- nls.builtins.formatting.trim_whitespace,
+      },
+
+      -- for mason-null-ls
+      ensure_installed = { 'stylua', 'jq', 'shfmt', 'shellcheck' },
+    }
+  end,
+
+  config = function(_, opts)
+    require('null-ls').setup(opts)
+    require('mason-null-ls').setup {
+      ensure_installed = opts.ensure_installed,
+    }
+  end,
+}
