4d3ad67aa3314f37a20ac6e180c895cadbde368d
Vitaly Kushner <vitaly@astrails.com>
2023-07-01 16:42:49 +0200

    bookmarks

---
+++ lua/plugins/editor/README.md
@@ -0,0 +1,13 @@
+## Bookmarks
+
+[vim-bookmarks](https://github.com/MattesGroeger/vim-bookmarks)
+
+- `mm` - Toggle Bookmark
+- `mc` - Clear Bookmarks in Buffer
+- `mx` - Clear Bookmark in All Buffers
+- `mn` - Next Bookmark
+- `mp` - Prev Bookmark
+- `ma` - Show All Bookmarks
+- `mi` - Add Bookmark Annotation
+- `mkk` - Move Bookmark Up
+- `mjj` - Move Bookmark Down
--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -7,4 +7,23 @@ return {
 
   -- REMEMBER LAST FILE LOCATION
   { 'ethanholz/nvim-lastplace', opts = {} }, -- https://github.com/ethanholz/nvim-lastplace
+
+  ----------------------------------------------------------------------------------------------
+  ----------------------------------------------------------------------------------------------
+  -- BOOKMARKS
+  {
+    'MattesGroeger/vim-bookmarks', -- https://github.com/MattesGroeger/vim-bookmarks
+    opts = {
+      bookmark_highlight_lines = true,
+      bookmark_center = true,
+      bookmark_display_annotation = true,
+      bookmark_auto_close = true,
+    },
+    config = function(_, opts)
+      vim.g.bookmark_highlight_lines = opts.bookmark_highlight_lines and 1 or 0
+      vim.g.bookmark_center = opts.bookmark_center and 1 or 0
+      vim.g.bookmark_display_annotation = opts.bookmark_display_annotation and 1 or 0
+      vim.g.bookmark_auto_close = opts.bookmark_auto_close and 1 or 0
+    end,
+  },
 }
