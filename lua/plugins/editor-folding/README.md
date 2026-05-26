033eaf42e1af625b031502178845bec9315cb757
Vitaly Kushner <vitaly@astrails.com>
2026-05-26 18:12:42 +0200

    replace folding plugin
    nvim-ufo was conflicting with refactoring by using require('async') and
    expecting it to come from a different place then refactoring

--- lua/plugins/editor-folding/README.md
+++ lua/plugins/editor-folding/README.md
@@ -2,22 +2,13 @@
 
 Modern folding with UFO (Unified Folding Option).
 
-- [nvim-ufo](https://github.com/kevinhwang91/nvim-ufo)
-- [promise-async](https://github.com/kevinhwang91/promise-async)
+- [nvim-origami](https://github.com/chrisgrieser/nvim-origami)
 
 ## Features
 
-- Treesitter-based folding
-- Modern arrow indicators
-- Fold column enabled
+- LSP based folding with follback to Treesitter and then to indent
 
 ## Keymaps
 
 - `zR` - Open all folds
 - `zM` - Close all folds
-
-## Configuration
-
-- Provider: treesitter + indent fallback
-- Fold level: 99
-- Fold column: 1
--- lua/plugins/editor-folding/init.lua
+++ lua/plugins/editor-folding/init.lua
@@ -1,26 +1,15 @@
 -- modern looking folding with arrows
 -- NOTE: The actual status column configuration is in lua/plugins/editor/init.lua
 return {
-  'kevinhwang91/nvim-ufo', -- https://github.com/kevinhwang91/nvim-ufo#installation
-
-  dependencies = { 'kevinhwang91/promise-async', 'nvim-treesitter/nvim-treesitter' },
-
-  -- NOTE: If BufReadPre is used here, folding fails with an error
-  event = { 'BufReadPost', 'BufNewFile' },
-
-  config = function()
-    vim.o.foldcolumn = '1' -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-
-    require('ufo').setup {
-      provider_selector = function()
-        return { 'treesitter', 'indent' }
-      end,
-    }
-
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-  end,
+  {
+    'chrisgrieser/nvim-origami', -- https://github.com/chrisgrieser/nvim-origami
+    enabled = true,
+    event = 'VeryLazy',
+    opts = {},
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+  },
 }
