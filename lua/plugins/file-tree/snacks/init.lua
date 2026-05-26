c902fdc9ffe5aa022d472d1183372cdbbaa420f1
Vitaly Kushner <vitaly@astrails.com>
2026-05-26 18:54:54 +0200

    use latest mini.pairs

--- lua/plugins/file-tree/snacks/init.lua
+++ lua/plugins/file-tree/snacks/init.lua
@@ -1,3 +1,5 @@
+_t('file-tree/snacks')
+
 local tools = require('lib.tools')
 
 ---@module 'snacks'
@@ -6,11 +8,30 @@ return tools.vplug({
   'plugins.file-tree.snacks',
 
   config = function()
+    _t('file-tree/snacks: config')
+
     local glue = require('glue').register('file-tree.snacks')
-    glue.handle(
-      'file-tree.actions.popup',
-      function() Snacks.picker.explorer({ layout = { preset = 'default', layout = { min_width = 40, width = 0.2 } }, jump = { close = true } }) end
-    )
+
+    -- Popup explorer with reveal - uses different source name so it doesn't conflict with sidebar
+    glue.handle('file-tree.actions.popup', function()
+      local file = vim.api.nvim_buf_get_name(0)
+      local Tree = require('snacks.explorer.tree')
+      local Actions = require('snacks.explorer.actions')
+
+      -- Open tree to current file before opening picker
+      if file ~= '' and vim.uv.fs_stat(file) then Tree:open(file) end
+
+      Snacks.picker.explorer({
+        source = 'explorer_popup', -- different source name prevents conflict with sidebar
+        layout = { preset = 'default', layout = { min_width = 40, width = 0.2 } },
+        jump = { close = true },
+        on_show = function(picker)
+          if file ~= '' then Actions.update(picker, { target = file, refresh = true }) end
+        end,
+      })
+    end)
+
+    -- Sidebar explorer
     glue.handle('file-tree.actions.toggle', function() Snacks.explorer.open() end)
     glue.handle('file-tree.actions.reveal', function() Snacks.explorer.reveal() end)
   end,
