f03373e8a465de7a1ca139542d49f4dd57d669c0
Vitaly Kushner <vitaly@astrails.com>
2026-05-28 02:16:20 +0200

    cleanups

--- lua/plugins/base-keymaps/utils.lua
+++ lua/plugins/base-keymaps/utils.lua
@@ -3,13 +3,11 @@ local UTIL = {}
 ---------------------------------------------------------------------------------
 -- redraw
 ---------------------------------------------------------------------------------
-function UTIL.redraw()
-  vim.cmd [[redraw]]
-end
+function UTIL.redraw() vim.cmd([[redraw]]) end
 
 ---------------------------------------------------------------------------------
 -- toggles
-local toggle = require 'lib.toggle'
+local toggle = require('lib.toggle')
 UTIL.toggle_concealcursor = toggle.toggler('o:concealcursor', { 'n', '' }, UTIL.redraw)
 UTIL.toggle_conceallevel = toggle.toggler('o:conceallevel', { 0, 1, 2 }, UTIL.redraw)
 UTIL.toggle_clipboard = toggle.toggler('o:clipboard', { 'unnamedplus', '' }, UTIL.redraw)
@@ -25,7 +23,7 @@ function UTIL.toggle_verboselog()
   else
     vim.o.verbose = 0
     vim.o.verbosefile = ''
-    print 'verbose off'
+    print('verbose off')
   end
   UTIL.redraw()
 end
@@ -34,21 +32,18 @@ end
 -- find next line with equal (or lower) indent level
 ---------------------------------------------------------------------------------
 function UTIL.next_indent(exclusive, fwd, lowerlevel, skipblanks)
-  local line = vim.fn.line '.'
-  local column = vim.fn.col '.'
-  local lastline = vim.fn.line '$'
+  local line = vim.fn.line('.')
+  local column = vim.fn.col('.')
+  local lastline = vim.fn.line('$')
   local indent = vim.fn.indent(line)
   local stepvalue = fwd and 1 or -1
   while line > 0 and line <= lastline do
     line = line + stepvalue
     if (not lowerlevel and vim.fn.indent(line) == indent) or (lowerlevel and vim.fn.indent(line) < indent) then
       if not skipblanks or #vim.fn.getline(line) > 0 then
-        if exclusive then
-          line = line - stepvalue
-        end
-        vim.notify('line: ' .. line .. ' column: ' .. column)
+        if exclusive then line = line - stepvalue end
         vim.cmd('' .. line)
-        vim.cmd 'normal ^'
+        vim.cmd('normal ^')
         return
       end
     end
